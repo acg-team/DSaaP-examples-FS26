91ff65400cf3bd72ed07c58a74072aaebad52923
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2026-03-20 17:01:44 +0100

    Flip strict assertion test file with fixed
    Fixed strict assertion example files because the fixed and non fixed were flipped

--- 5_testing/unit_test_pitfalls/assertion_too_strict.py
+++ 5_testing/unit_test_pitfalls/assertion_too_strict.py
@@ -1,10 +1,5 @@
-import pytest
-
 def discounted_price(price: float, percentage: float) -> float:
     return max(0, price - price * percentage / 100)
 
-def test_discount_float_precision_approx():
-    assert discounted_price(10.75, 100/3) == pytest.approx(7.1666666667)
-
-def test_discount_float_precision_approx_rounding():
-    assert discounted_price(10.75, 100/3) == pytest.approx(7.1666666667, rel=1e-2)
+def test_discount_float_precision_too_strict():
+    assert discounted_price(10.75, 100/3) == 7.1666666667
