286543419ccd148915c09d1f6609ff358a31cb76
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2026-03-21 20:08:32 +0100

    Uncomment line in manual testing example

--- 5_testing/why_unit_test/2_discount_introduced_bug.py
+++ 5_testing/why_unit_test/2_discount_introduced_bug.py
@@ -4,4 +4,4 @@ def discounted_price(price: float, percentage: float) -> float:
 if __name__ == "__main__":
     print(discounted_price(100, 20)) # expected output: 80.0
 
-    # print(discounted_price(200, 20)) # expected output: 160.0, but will print 180.0 due to the bug in the discount function
+    print(discounted_price(200, 20)) # expected output: 160.0, but will print 180.0 due to the bug in the discount function
