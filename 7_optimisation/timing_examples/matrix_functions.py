36063cb21e38fc39f0d5a1fd32d73f9acd159f0e
Jūlija Pečerska <julija.pecerska@zhaw.ch>
2026-04-15 19:56:48 +0200

    Rename matrix functions for clarity

--- 7_optimisation/timing_examples/matrix_functions.py
+++ 7_optimisation/timing_examples/matrix_functions.py
@@ -11,7 +11,7 @@ def do_math(value: float) -> float:
     return m.log((value / 2.0) + 1.0)
 
 
-def do_math_row_column(matrix: list[list[float]]):
+def do_math_row_major(matrix: list[list[float]]):
     """
     Applies the do_math function to each element of the given matrix in place,
     iterating row by row and column by column.
@@ -47,7 +47,7 @@ def do_numpy_math_row_column(matrix: list[list[np.float64]]):
     # 5000x5000 matrix: 15.438082575798035 seconds
 
 
-def divide_by_two_row_column(matrix: list[list[float]]):
+def divide_by_two_row_major(matrix: list[list[float]]):
     """
     Divides each element of the given matrix by 2 in place, iterating row by row and column by column.
     """
@@ -59,7 +59,7 @@ def divide_by_two_row_column(matrix: list[list[float]]):
             matrix[i][j] /= 2.0
     # 5000x5000 matrix: 1.2901440143585206 seconds
 
-def divide_by_two_column_row(matrix: list[list[float]]):
+def divide_by_two_column_major(matrix: list[list[float]]):
     """
     Divides each element of the given matrix by 2 in place, iterating column by column and row by row.
     """
@@ -82,16 +82,16 @@ def compare_matrix_operations():
     size = 1000
     runs = 10
     print(f"Matrix operations with a {size}x{size} matrix over {runs} runs")
-    report_performance(runs, divide_by_two_row_column, [[float(i) for i in range(1, size)] for _ in range(size)])
-    report_performance(runs, divide_by_two_column_row, [[float(i) for i in range(1, size)] for _ in range(size)])
+    report_performance(runs, divide_by_two_row_major, [[float(i) for i in range(1, size)] for _ in range(size)])
+    report_performance(runs, divide_by_two_column_major, [[float(i) for i in range(1, size)] for _ in range(size)])
     report_performance(runs, divide_by_two_numpy, np.array([[float(i) for i in range(1, size)] for _ in range(size)]))
 
     print("\nComparing division by 2 and more complex math operations:")
-    report_performance(runs, divide_by_two_row_column, [[float(i) for i in range(1, size)] for _ in range(size)])
-    report_performance(runs, do_math_row_column, [[float(i) for i in range(1, size)] for _ in range(size)])
+    report_performance(runs, divide_by_two_row_major, [[float(i) for i in range(1, size)] for _ in range(size)])
+    report_performance(runs, do_math_row_major, [[float(i) for i in range(1, size)] for _ in range(size)])
 
     print("\nComparing math operations with and without numpy:")
-    report_performance(runs, do_math_row_column, [[float(i) for i in range(1, size)] for _ in range(size)])
+    report_performance(runs, do_math_row_major, [[float(i) for i in range(1, size)] for _ in range(size)])
     report_performance(runs, do_numpy_math_row_column, [[np.float64(i) for i in range(1, size)] for _ in range(size)])
 
 
